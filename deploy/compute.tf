8625aa86bf9da062e05a7dc30ef64fa0fb8d6bae
Matthew Lau <matthew.lau@matthews-mbp.home>
2021-07-31 19:06:01 +0100

    Add Terraform config

---
+++ deploy/compute.tf
@@ -0,0 +1,8 @@
+resource "google_container_cluster" "app_cluster" {
+  name       = "react-gke-cluster"
+  location   = var.region
+
+  enable_autopilot = true
+
+  
+}
---
+++ deploy/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.71.0"
+    }
+  }
+}
+
+terraform {
+  backend "gcs" {
+    bucket = "react-gke-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  region      = var.region
+  zone        = var.zone
+  project     = var.project
+}
