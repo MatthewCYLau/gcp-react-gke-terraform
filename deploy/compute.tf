e1d563a0b7a4bbb0424c191f55af9f19a5637813
Matthew Lau <lau.cy.matthew@gmail.com>
2021-08-01 13:07:12 +0100

    Tidy K8 config

--- deploy/compute.tf
+++ deploy/compute.tf
@@ -2,6 +2,9 @@ resource "google_container_cluster" "app_cluster" {
   name             = "react-gke-cluster"
   location         = var.region
   enable_autopilot = true
+  vertical_pod_autoscaling {
+    enabled = true
+  }
 }
 
 resource "google_compute_global_address" "external_static_ip" {
@@ -9,5 +12,10 @@ resource "google_compute_global_address" "external_static_ip" {
   address_type = "EXTERNAL"
   ip_version   = "IPV4"
   project      = var.project
+  description  = "External static IP address for React GKE app"
+}
+
+output "app_ip" {
+  value       = google_compute_global_address.external_static_ip.address
   description  = "External static IP address for React GKE app"
 }
